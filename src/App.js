a48cf234861e4b0f2fef3f7e3002bfc1a9dee581
Deisy <trujillosanchezgianina@gmail.com>
2023-06-15 12:13:37 -0500

    post message trato de arreglar

--- src/App.js
+++ src/App.js
@@ -5,13 +5,35 @@ import Home from "./Pages/Home";
 import NewCategoria from "./Pages/NewCategoria";
 import NewVideo from "./Pages/NewVideo";
 import Error404 from "./Pages/error404";
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { buscar } from './api/api';
-function App() {
 
+function App() {
   const [categorias, setCategorias] = useState([]);
+
   useEffect(() => {
     fetchCategorias();
+  }, []);
+
+  useEffect(() => {
+    // Función para recibir mensajes
+    const receiveMessage = (event) => {
+      // Verificar el origen del mensaje
+      if (event.origin !== 'https://heflix-lac.vercel.app') {
+        return;
+      }
+
+      // Realizar acciones con los datos del mensaje
+      console.log('Mensaje recibido:', event.data);
+    };
+
+    // Agregar un event listener para escuchar los mensajes
+    window.addEventListener('message', receiveMessage);
+
+    // Limpiar el event listener al desmontar el componente
+    return () => {
+      window.removeEventListener('message', receiveMessage);
+    };
   }, []);
 
   const fetchCategorias = async () => {
@@ -24,26 +46,38 @@ function App() {
       console.error(error);
     }
   };
-  
+
   const addCategoria = (categoria) => {
     console.log(categoria);
-    const nuevaLista = [...categorias, categoria]
-    setCategorias(nuevaLista)
-    fetchCategorias();// Llama a fetchCategorias para actualizar los datos
-  }
-  
-  return <>
-    <Router>
-      <GlobalStyle />
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/NewCategorie' element={<NewCategoria addCategoria={addCategoria}  fetch={fetchCategorias}/>} />
-        <Route path='/NewVideo' element={<NewVideo  addCategoria={addCategoria}/>} />
-        <Route path='*' element={<Error404 />} />
-      </Routes>
-    </Router>
-  </>
+    const nuevaLista = [...categorias, categoria];
+    setCategorias(nuevaLista);
+    fetchCategorias(); // Llama a fetchCategorias para actualizar los datos
+  };
+
+  // Función para enviar mensajes
+  const sendMessage = () => {
+    const message = 'Hola, esta es una prueba de mensaje';
+    const targetOrigin = 'https://www.youtube.com'; // Reemplaza con el origen correcto del destino
+
+    // Enviar el mensaje al destino especificado
+    window.parent.postMessage(message, targetOrigin);
+  };
+
+  return (
+    <>
+      <Router>
+        <GlobalStyle />
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/NewCategorie' element={<NewCategoria addCategoria={addCategoria} fetch={fetchCategorias} />} />
+          <Route path='/NewVideo' element={<NewVideo addCategoria={addCategoria} />} />
+          <Route path='*' element={<Error404 />} />
+        </Routes>
+      </Router>
+      <button onClick={sendMessage}>Enviar Mensaje</button> {/* Agrega un botón para enviar el mensaje */}
+    </>
+  );
 }
 
 export default App;
